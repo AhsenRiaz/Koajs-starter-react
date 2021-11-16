93e217261773da5dca26887275bdc08b4f7c3922
ahsenrz <muhammadahsenriaz@gmail.com>
2021-11-16 19:52:41 +0500

    Updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,12 @@ import * as json from "koa-json";
 import * as Router from "koa-router";
 import * as bodyParser from "koa-bodyparser";
 import * as cors from "@koa/cors";
+import * as morgan from "morgan";
+import * as createErrors from "http-errors";
+require("dotenv").config();
+import { Context } from "koa";
 
-const port: number = 5000;
+const port = process.env.PORT || 5000 ;
 const app = new Koa();
 const router = new Router();
 
@@ -22,18 +26,26 @@ app.use(cors());
 router.get("/", index);
 router.post("/add", add);
 
-async function index(ctx: any) {
-  ctx.body = {
-    message: "hello Response",
-  };
+async function index(ctx: Context) {
+  try {
+    ctx.response.body = {
+      message: "hello Response",
+    };
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // Add thing
-async function add(ctx: any) {
-  const task = await ctx.request.body.name;
-  things.push({name:task});
-  ctx.body = {
-      data : things
+async function add(ctx: Context) {
+  try {
+    const task = await ctx.request.body.name;
+    things.push({ name: task });
+    ctx.response.body = {
+      data: things,
+    };
+  } catch (err) {
+    console.log(err);
   }
 }
 
@@ -42,4 +54,4 @@ async function add(ctx: any) {
 // ROUTER MIDDLEWARE
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(port, () => console.log("Server started..."));
+app.listen(port, () => console.log(`Server started on port ${port}`));
